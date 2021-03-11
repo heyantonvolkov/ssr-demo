89c72f4745af67fd4f765541c5256661a10880ce
Anton <Tonyfirehead@gmail.com>
2021-03-12 01:31:57 +0300

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent,
   ],
   imports: [
-    RouterModule.forRoot([{ path: 'first', loadChildren: () => import('./first/first.module').then(m => m.FirstModule) }]),
+    RouterModule.forRoot([{ path: 'first', loadChildren: () => import('./first/first.module').then(m => m.FirstModule) }], { relativeLinkResolution: 'legacy' }),
     BrowserModule.withServerTransition({ appId: 'serverApp' })
   ],
   providers: [],
